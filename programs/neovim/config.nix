828fbde41df979123181b6da4b57eab24d2e5f8d
mstream <maciej.laciak@gmail.com>
2020-11-26 06:50:22 +0000

    Add various coc extensions

--- programs/neovim/config.nix
+++ programs/neovim/config.nix
@@ -103,6 +103,7 @@ xmap <silent> <C-s> <Plug>(coc-range-select)
 command! -nargs=0 Format :call CocAction('format')
 command! -nargs=? Fold :call     CocAction('fold', <f-args>)
 command! -nargs=0 OR   :call     CocAction('runCommand', 'editor.action.organizeImport')
+command! -nargs=0 Prettier :CocCommand prettier.formatFile
 
 nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
 nnoremap <silent><nowait> <space>e  :<C-u>CocList extensions<cr>
--- programs/neovim/plugins.nix
+++ programs/neovim/plugins.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 with pkgs.vimPlugins; [
+  coc-emmet
   coc-git
   coc-highlight
   coc-html
@@ -8,9 +9,13 @@ with pkgs.vimPlugins; [
   coc-json
   coc-markdownlint
   coc-nvim
+  coc-pairs
+  coc-prettier
   coc-spell-checker
   coc-yaml
   dhall-vim
+  emmet-vim
+  fzf-vim
   purescript-vim
   vim-airline
   vim-surround
