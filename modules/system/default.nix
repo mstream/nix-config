6650e1dda056adcefa45ec9780e9fa6c9480e228
mstream <maciej.laciak@gmail.com>
2022-01-15 22:43:52 +0000

    add mac dock settings

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,6 +1,23 @@
 _: {
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.autohide-delay = "0.25";
+  system.defaults.dock.autohide-time-modifier = "1.0";
+  system.defaults.dock.expose-animation-duration = "1.0";
+  system.defaults.dock.expose-group-by-app = true;
+  system.defaults.dock.launchanim = true;
+  system.defaults.dock.mineffect = "genie";
+  system.defaults.dock.minimize-to-application = true;
+  system.defaults.dock.mouse-over-hilite-stack = true;
+  system.defaults.dock.mru-spaces = true;
+  system.defaults.dock.orientation = "bottom";
+  system.defaults.dock.show-process-indicators = true;
+  system.defaults.dock.showhidden = true;
+  system.defaults.dock.show-recents = false;
+  system.defaults.dock.static-only = false;
+  system.defaults.dock.tilesize = 32;
   system.keyboard = {
     enableKeyMapping = true;
     remapCapsLockToEscape = true;
   };
+  system.stateVersion = 4;
 }
