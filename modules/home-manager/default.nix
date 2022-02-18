b29c6b2385ee01539eefa70cc31ab4f648f4cd19
mstream <maciej.laciak@gmail.com>
2022-02-18 13:00:17 +0000

    add empty development directories generation

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,20 +1,43 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  userConfig = username: {
+    home.file.ownProjects = {
+      recursive = true;
+      target = "Development/projects/${username}/.keep";
+      text = "";
+    };
 
+    home.file.skyProjects = {
+      recursive = true;
+      target = "Development/projects/sky-uk/.keep";
+      text = "";
+    };
+
+    home.file.otherProjects = {
+      recursive = true;
+      target = "Development/projects/other/.keep";
+      text = "";
+    };
+
+    programs = {
+      alacritty = import ./alacritty/default.nix;
+      bat = import ./bat/default.nix;
+      firefox = (import ./firefox/default.nix { inherit pkgs; });
+      git = import ./git/default.nix;
+      gpg = import ./gpg/default.nix;
+      jq = import ./jq/default.nix;
+      neovim = (import ./neovim/default.nix { inherit pkgs; });
+      password-store = import ./password-store/default.nix;
+      tmux = import ./tmux/default.nix;
+      zsh = import ./zsh/default.nix;
+    };
+  };
+in
+{
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
   };
 
-  home-manager.users.mstream.programs = {
-    alacritty = import ./alacritty/default.nix;
-    bat = import ./bat/default.nix;
-    firefox = (import ./firefox/default.nix { inherit pkgs; });
-    git = import ./git/default.nix;
-    gpg = import ./gpg/default.nix;
-    jq = import ./jq/default.nix;
-    neovim = (import ./neovim/default.nix { inherit pkgs; });
-    password-store = import ./password-store/default.nix;
-    tmux = import ./tmux/default.nix;
-    zsh = import ./zsh/default.nix;
-  };
+  home-manager.users.mstream = userConfig "mstream";
 }
